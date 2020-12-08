63a775f2cfa6f0652d5e755a04a45240bbc5987c
fengjk <1327246148@qq.com>
2020-12-08 15:40:42 +0800

    3-2 start

---
+++ 3-2/client.cpp
@@ -0,0 +1,239 @@
+//
+// Created by fengjk on 2020/12/3.
+//
+#pragma comment(lib, "Ws2_32.lib")
+
+#include <iostream>
+#include <winsock2.h>
+#include <stdio.h>
+#include <fstream>
+#include <vector>
+#include <time.h>
+
+using namespace std;
+const int Mlenx = 509;
+const unsigned char ACK = 0x03;
+const unsigned char NAK = 0x07;
+const unsigned char LAST_PACK = 0x18;
+const unsigned char NOTLAST_PACK = 0x08;
+const unsigned char SHAKE_1 = 0x01;
+const unsigned char SHAKE_2 = 0x02;
+const unsigned char SHAKE_3 = 0x04;
+const unsigned char WAVE_1 = 0x80;
+const unsigned char WAVE_2 = 0x40;
+const int TIMEOUT = 500;//毫秒
+char buffer[200000000];
+int len;
+
+SOCKET client;
+SOCKADDR_IN serverAddr, clientAddr;
+
+unsigned char sum_cal(char *arr, int lent) {
+    if (lent == 0)
+        return ~(0);
+    unsigned char ret = arr[0];
+
+    for (int i = 1; i < lent; i++) {
+        unsigned int tmp = ret + (unsigned char) arr[i];
+        tmp = tmp / (1 << 8) + tmp % (1 << 8);
+        tmp = tmp / (1 << 8) + tmp % (1 << 8);
+        ret = tmp;
+    }
+    return ~ret;
+}
+void wave_hand() {
+    int tot_fail = 0;
+    while (1) {
+        //发送wave_1
+        char tmp[2];
+        tmp[1] = WAVE_1;
+        tmp[0] = sum_cal(tmp + 1, 1);
+        sendto(client, tmp, 2, 0, (sockaddr *) &serverAddr, sizeof(serverAddr));
+        int begintime = clock();
+        char recv[2];
+        int lentmp = sizeof(serverAddr);
+        int fail_send = 0;
+        while (recvfrom(client, recv, 2, 0, (sockaddr *) &serverAddr, &lentmp) == SOCKET_ERROR)
+            if (clock() - begintime > TIMEOUT) {
+                fail_send = 1;
+                tot_fail++;
+                break;
+            }
+        //接受wave_2并校验
+        if (fail_send == 0 && sum_cal(recv, 2) == 0 && recv[1] == WAVE_2)
+            break;
+        else {
+            if (tot_fail == 3) {
+                printf("断开失败，正在自动释放资源...");
+                break;
+            }
+            continue;
+        }
+    }
+}
+
+
+bool send_package(char *message, int lent, int order, int last = 0) {
+    if (lent > Mlenx) {
+        return false;
+    }
+    if (last == false && lent != Mlenx) {
+        return false;
+    }
+    char *tmp;
+    int tmp_len;
+
+    if (last) {
+        tmp = new char[lent + 4];
+        tmp[1] = LAST_PACK;
+        tmp[2] = order;
+        tmp[3] = lent;
+        for (int i = 4; i < lent + 4; i++)
+            tmp[i] = message[i - 4];
+        tmp[0] = sum_cal(tmp + 1, lent + 3);
+        tmp_len = lent + 4;
+    } else {
+        tmp = new char[lent + 3];
+        tmp[1] = NOTLAST_PACK;
+        tmp[2] = order;
+        for (int i = 3; i < lent + 3; i++)
+            tmp[i] = message[i - 3];
+        tmp[0] = sum_cal(tmp + 1, lent + 2);
+        tmp_len = lent + 3;
+    }
+    int send_to_cnt=0;
+    while (1) {
+        send_to_cnt++;
+        sendto(client, tmp, tmp_len, 0, (sockaddr *) &serverAddr, sizeof(serverAddr));
+        int begintime = clock();
+        char recv[3];
+        int lentmp = sizeof(serverAddr);
+        int fail_send = 0;
+        while (recvfrom(client, recv, 3, 0, (sockaddr *) &serverAddr, &lentmp) == SOCKET_ERROR)
+            if (clock() - begintime > TIMEOUT) {
+                fail_send = 1;
+                if(send_to_cnt==5)
+                {
+                    wave_hand();
+                    return 0;
+                }
+                break;
+            }
+        if (fail_send == 0 && sum_cal(recv, 3) == 0 && recv[1] == ACK && recv[2] == (char)order)
+            return true;
+    }
+}
+
+
+void shake_hand() {
+    while (1) {
+        //发送shake_1
+        char tmp[2];
+        tmp[1] = SHAKE_1;
+        tmp[0] = sum_cal(tmp + 1, 1);
+        sendto(client, tmp, 2, 0, (sockaddr *) &serverAddr, sizeof(serverAddr));
+        int begintime = clock();
+        char recv[2];
+        int lentmp = sizeof(clientAddr);
+        int fail_send = 0;
+        while (recvfrom(client, recv, 2, 0, (sockaddr *) &serverAddr, &lentmp) == SOCKET_ERROR)
+            if (clock() - begintime > TIMEOUT) {
+                fail_send = 1;
+                break;
+            }
+        //接受shake_2并校验
+        if (fail_send == 0 && sum_cal(recv, 2) == 0 && recv[1] == SHAKE_2) {
+            {
+                //发送shake_3
+                tmp[1] = SHAKE_3;
+                tmp[0] = sum_cal(tmp + 1, 1);
+                sendto(client, tmp, 2, 0, (sockaddr *) &serverAddr, sizeof(serverAddr));
+                break;
+            }
+        }
+    }
+}
+
+
+void send_message(char *message, int lent) {
+    int package_num = lent / Mlenx + (lent % Mlenx != 0);
+    static int order = 0;
+    for (int i = 0; i < package_num; i++) {
+
+        send_package(message + i * Mlenx, i == package_num - 1 ? lent - (package_num - 1) * Mlenx : Mlenx, order++,
+                     i == package_num - 1);
+
+        order=order%(1<<8);
+        if (i % 100 == 0)
+            printf("此文件已经发送%.2f%%\n", (float) i / package_num*100);
+    }
+}
+
+
+int main() {
+    //设置非阻塞
+
+    WSADATA wsadata;
+    int error = WSAStartup(MAKEWORD(2, 2), &wsadata);
+    if (error) {
+        printf("init error");
+        return 0;
+    }
+    string serverip;
+    while (1) {
+        printf("请输入接收方ip地址:\n");
+        getline(cin, serverip);
+
+        if (inet_addr(serverip.c_str()) == INADDR_NONE) {
+            printf("ip地址不合法，555~\n");
+            continue;
+        }
+        break;
+    }
+
+    int port = 11451;
+
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(port);
+    serverAddr.sin_addr.s_addr = inet_addr(serverip.c_str());
+    client = socket(AF_INET, SOCK_DGRAM, 0);
+
+    int time_out=1;//1ms超时
+    setsockopt(client, SOL_SOCKET, SO_RCVTIMEO, (char *)&time_out, sizeof(time_out));
+
+    if (client == INVALID_SOCKET) {
+        printf("creat udp socket error");
+        return 0;
+    }
+    string filename;
+    while (1) {
+        printf("请输入要发送的文件名：");
+        cin >> filename;
+        ifstream fin(filename.c_str(), ifstream::binary);
+        if (!fin) {
+            printf("文件未找到，555~\n");
+            continue;
+        }
+        unsigned char t = fin.get();
+        while (fin) {
+            buffer[len++] = t;
+            t = fin.get();
+        }
+        fin.close();
+        break;
+    }
+    printf("链接建立中...\n");
+    shake_hand();
+    printf("链接建立完成。 \n正在发送信息...\n");
+    send_message((char *) (filename.c_str()), filename.length());
+    printf("文件名发送完毕，正在发送文件内容...\n");
+    send_message(buffer, len);
+    printf("文件内容发送完毕。\n 开始断开连接...\n");
+    wave_hand();
+    printf("连接已断开。");
+    closesocket(client);
+    WSACleanup();
+
+    return 0;
+}
+
---
+++ 3-2/server.cpp
@@ -0,0 +1,181 @@
+#pragma comment(lib, "Ws2_32.lib")
+
+#include <iostream>
+#include <winsock2.h>
+#include <stdio.h>
+#include <fstream>
+#include <vector>
+#include<time.h>
+
+using namespace std;
+
+using namespace std;
+const int Mlenx = 509;
+const unsigned char ACK = 0x03;
+const unsigned char NAK = 0x07;
+const unsigned char LAST_PACK = 0x18;
+const unsigned char NOTLAST_PACK = 0x08;
+const unsigned char SHAKE_1 = 0x01;
+const unsigned char SHAKE_2 = 0x02;
+const unsigned char SHAKE_3 = 0x04;
+const unsigned char WAVE_1 = 0x80;
+const unsigned char WAVE_2 = 0x40;
+const int TIMEOUT = 500;//毫秒
+char buffer[200000000];
+int len;
+
+SOCKADDR_IN serverAddr, clientAddr;
+SOCKET server; //选择udp协议
+
+
+unsigned char sum_cal(char *arr, int lent) {
+    if (lent == 0)
+        return ~(0);
+    unsigned char ret = arr[0];
+
+    for (int i = 1; i < lent; i++){
+        unsigned int tmp = ret + (unsigned char) arr[i];
+        tmp = tmp / (1 << 8) + tmp % (1 << 8);
+        tmp = tmp / (1 << 8) + tmp % (1 << 8);
+        ret = tmp;
+    }
+    return ~ret;
+}
+
+void wait_shake_hand() {
+    while (1) {
+        char recv[2];
+        int connect = 0;
+        int lentmp = sizeof(clientAddr);
+        while (recvfrom(server, recv, 2, 0, (sockaddr *) &clientAddr, &lentmp) == SOCKET_ERROR);
+        if (sum_cal(recv, 2) != 0 || recv[1] != SHAKE_1)
+            continue;
+
+        while (1) {
+            recv[1] = SHAKE_2;
+            recv[0] = sum_cal(recv + 1, 1);
+            sendto(server, recv, 2, 0, (sockaddr *) &clientAddr, sizeof(clientAddr));
+            while (recvfrom(server, recv, 2, 0, (sockaddr *) &clientAddr, &lentmp) == SOCKET_ERROR);
+            if (sum_cal(recv, 2) == 0 && recv[1] == SHAKE_1)
+                continue;
+            if (sum_cal(recv, 2) != 0 || recv[1] != SHAKE_3) {
+                printf("链接建立失败，请重启客户端。");
+                connect = 1;
+            }
+            break;
+        }
+        if (connect == 1)
+            continue;
+        break;
+    }
+}
+
+void wait_wave_hand() {
+    while (1) {
+        char recv[2];
+        int lentmp = sizeof(clientAddr);
+        while (recvfrom(server, recv, 2, 0, (sockaddr *) &clientAddr, &lentmp) == SOCKET_ERROR);
+        if (sum_cal(recv, 2) != 0 || recv[1] != (char)WAVE_1)
+            continue;
+        recv[1] = WAVE_2;
+        recv[0] = sum_cal(recv + 1, 1);
+        sendto(server, recv, 2, 0, (sockaddr *) &clientAddr, sizeof(clientAddr));
+        break;
+    }
+}
+
+void recv_message(char *message, int &len_recv) {
+    char recv[Mlenx + 4];
+    int lentmp = sizeof(clientAddr);
+    static char last_order = -1;
+    len_recv = 0;
+    while (1) {
+        while (1) {
+            memset(recv,0,sizeof(recv));
+            while (recvfrom(server, recv, Mlenx + 4, 0, (sockaddr *) &clientAddr, &lentmp) == SOCKET_ERROR);
+            char send[3];
+            if (sum_cal(recv, Mlenx + 4) == 0) {
+                send[1] = ACK;
+                send[2] = recv[2];
+                send[0] = sum_cal(send + 1, 2);
+                sendto(server, send, 3, 0, (sockaddr *) &clientAddr, sizeof(clientAddr));
+                break;
+            } else {
+                send[1] = NAK;
+                send[2] = recv[2];//NAK序号可能不准确
+                send[0] = sum_cal(send + 1, 2);
+                sendto(server, send, 3, 0, (sockaddr *) &clientAddr, sizeof(clientAddr));
+                cout << "NAK" << endl;
+                continue;
+            }
+        }
+        if (last_order == recv[2])
+            continue; //和上一个数据包序号相同，丢弃
+        last_order = recv[2];
+//        printf("%d\n",last_order);
+        if (LAST_PACK == recv[1]) {
+            for (int i = 4; i < recv[3] + 4; i++)
+                message[len_recv++] = recv[i];
+
+            break;
+        } else {
+            for (int i = 3; i < Mlenx + 3; i++)
+                message[len_recv++] = recv[i];
+        }
+    }
+}
+
+int main() {
+    //设置非阻塞
+
+    WSADATA wsadata;
+    int nError = WSAStartup(MAKEWORD(2, 2), &wsadata);
+    if (nError) {
+        printf("start error\n");
+        return 0;
+    }
+
+    int Port = 11451;
+
+    serverAddr.sin_family = AF_INET; //使用ipv4
+    serverAddr.sin_port = htons(Port); //端口
+    serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    server = socket(AF_INET, SOCK_DGRAM, 0);
+
+    int time_out=1;//1ms超时
+    setsockopt(server, SOL_SOCKET, SO_RCVTIMEO, (char *)&time_out, sizeof(time_out));
+
+    if (server == INVALID_SOCKET) {
+        printf("create fail");
+        closesocket(server);
+        return 0;
+    }
+    int retVAL = bind(server, (sockaddr *) (&serverAddr), sizeof(serverAddr));
+    if (retVAL == SOCKET_ERROR) {
+        printf("bind fail");
+        closesocket(server);
+        WSACleanup();
+        return 0;
+    }
+
+    //等待接入
+    printf("等待接入...\n");
+    wait_shake_hand();
+    printf("用户已接入。\n正在接收数据...\n");
+    recv_message(buffer, len);
+    printf("第一条信息接收成功。");
+    buffer[len] = 0;
+    cout << buffer << endl;
+    string file_name(buffer);
+    printf("开始接收第二条信息...\n");
+    recv_message(buffer, len);
+    printf("第二条信息接收成功。\n");
+    ofstream fout(file_name.c_str(), ofstream::binary);
+    for (int i = 0; i < len; i++)
+        fout << buffer[i];
+    fout.close();
+    wait_wave_hand();
+    printf("链接断开");
+    return 0;
+}
